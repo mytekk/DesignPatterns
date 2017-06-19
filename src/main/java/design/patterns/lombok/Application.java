15e1d44780c5030e28d55c7cfccc753aa7730984
Bartłomiej Mytko <bartlomiej.mytko@gmail.com>
2017-06-19 12:44:45 +0200

    Lombok - AllArgsConstructor and some stream and lambda example

--- src/main/java/design/patterns/lombok/Application.java
+++ src/main/java/design/patterns/lombok/Application.java
@@ -2,6 +2,9 @@ package design.patterns.lombok;
 
 import design.patterns.lombok.User;
 
+import java.util.Arrays;
+import java.util.List;
+
 /**
  * Created by mytek on 2017-06-14.
  */
@@ -16,5 +19,19 @@ public class Application {
                 .zipCode("60-123")
                 .build();
         System.out.println(user);
+
+        System.out.println("===========");
+
+
+        List<String> values = Arrays.asList("Szymon;Nowak;+48 12345678;23;60-123", "Michal;Wisniewski;987654345;33;60-456", "Anna;Kowalska;94726384;45;60-789");
+
+        values.stream()
+                .map(e -> {
+                    String[] split = e.split(";");
+                    return new User(split[0], split[1], split[2], Integer.parseInt(split[3]), split[4]);
+                })
+                .filter(e -> e.getAge() > 25)
+                .forEach(System.out::println);
+
     }
 }
--- src/main/java/design/patterns/lombok/User.java
+++ src/main/java/design/patterns/lombok/User.java
@@ -1,5 +1,6 @@
 package design.patterns.lombok;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.Builder;
 
@@ -17,6 +18,8 @@ import lombok.Builder;
  * kompilator wygeneruje statyczną zagniezdzoną klasę buildera oraz niezbędny
  * konstruktor w głównej klasie
  *
+ * @AllArgsConstructor - generuje konstruktor z listą parametrów tożsamą z listą pól w klasie
+ *
  * UWAGA - dzięki temu działąnie programu w tej paczce (package lombok) jest IDENTYCZNE z tym co
  * jest w paczce builder (package builder) - a nie trzeba było pisać tego wszystkiego ręcznie !!!
  *
@@ -27,6 +30,7 @@ import lombok.Builder;
 
 @Data
 @Builder
+@AllArgsConstructor
 public class User {
 
     private String firstName;
